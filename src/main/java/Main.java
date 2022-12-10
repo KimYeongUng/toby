47212e95769580fa7965b0bef73c556b0ae053ba
kimyeongung <kimyeongung92@gmail.com>
2022-12-10 15:23:55 +0900

    Use ApplicationContext

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-import dao.ConnectionMaker;
-import dao.DConnectionMaker;
+import dao.DaoFactory;
 import dao.UserDao;
 import domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -9,13 +10,14 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
         User user = new User();
         user.setId("1");
         user.setName("KIM");
         user.setPassword("1234");
 
-        ConnectionMaker conn = new DConnectionMaker();
-        UserDao dao = new UserDao(conn);
+        UserDao dao = context.getBean("userDao",UserDao.class);
         int ret = dao.deleteAll();
         System.out.println("DELETE RET:"+ret);
         dao.add(user);
